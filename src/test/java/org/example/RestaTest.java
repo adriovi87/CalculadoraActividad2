503506c44754c4e030af275510747a191b4fb1bc
Tu Nombre <tuemail@example.com>
2025-02-16 22:51:35 +0100

    Movido RestaTest.java a la carpeta correcta de tests

---
+++ src/test/java/org/example/RestaTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void restarReales() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void restarEnteros() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void restaTresReales() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void restarAcumulado() {
+    }
+}
