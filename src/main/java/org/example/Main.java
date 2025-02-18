637f7fa4557b420002eb8f8699b4bb6cdd2e6ad5
Manuel Marcote <manuelmarcotecodesido@gmail.com PROVISIONAL>
2025-02-18 14:25:00 +0100

    ADD: clase Main menú de calculadora y menú de operación

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,158 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        menuCalculadora();
+        scanner.close();
+    }
+    private static void menuCalculadora() {
+        int opcion;
+
+        do {
+            System.out.println("\n=== Menu de calculadora ===");
+            System.out.println("1. Suma");
+            System.out.println("2. Resta");
+            System.out.println("3. Producto");
+            System.out.println("4. Cociente");
+            System.out.println("0. Salir");
+            System.out.print("\nMarque el numero de una de las opciones: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+
+                case 1:
+                    menuSuma();
+                    break;
+
+                case 2:
+                    menuResta();
+                    break;
+
+                case 3:
+                    menuProducto();
+                    break;
+
+                case 4:
+                    menuCociente();
+                    break;
+
+                case 0:
+                    System.out.println("\n=== HASTA LA PROXIMA ===");
+                    break;
+
+                default:
+                    System.out.println("\nOPCION NO DISPONIBLE");
+                    break;
+            }
+        } while (opcion != 0);
+
+
+    }
+
+    private static void menuSuma() {
+
+    }
+
+    private static void menuResta() {
+
+    }
+
+    private static void menuProducto() {
+
+        int opcion3;
+
+        do {
+
+            System.out.println("\n=== Menu Producto ===");
+            System.out.println("1. Producto de dos numeros reales");
+            System.out.println("2. Producto de dos numeros enteros");
+            System.out.println("3. Producto de tres numeros reales");
+            System.out.println("4. Potencia");
+            System.out.println("5. Volver al menu de calculadora");
+            System.out.print("\nMarque el numero de una de las opciones para " +
+                    "calcular un producto: ");
+            opcion3 = scanner.nextInt();
+
+            switch (opcion3) {
+
+                case 1: {
+                    System.out.print("\nIndique el multiplicando real: ");
+                    double multiplicando = scanner.nextDouble();
+                    System.out.print("Indique el multiplicador real: ");
+                    double multiplicador = scanner.nextDouble();
+
+                    Producto producto = new Producto();
+                    double resultado = producto.multiplicar(multiplicando, multiplicador);
+                    System.out.print("El resultado es: " + resultado + "\n");
+
+                    break;
+                }
+
+                case 2: {
+                    System.out.print("\nIndique el multiplicando entero: ");
+                    int multiplicando = scanner.nextInt();
+                    System.out.print("Indique el multiplicador entero: ");
+                    int multiplicador = scanner.nextInt();
+
+                    Producto producto = new Producto();
+                    int resultado = producto.multiplicar(multiplicando, multiplicador);
+                    System.out.print("El resultado es: " + resultado + "\n");
+
+                    break;
+
+                }
+
+                case 3: {
+                    System.out.print("\nIndique el multiplicando real: ");
+                    double multiplicando = scanner.nextDouble();
+                    System.out.print("Indique el primer multiplicador real: ");
+                    double multiplicador1 = scanner.nextDouble();
+                    System.out.print("Indique el segundo multiplicador real: ");
+                    double multiplicador2 = scanner.nextDouble();
+
+                    Producto producto = new Producto();
+                    double resultado = producto.multiplicar(multiplicando, multiplicador1, multiplicador2);
+                    System.out.print("El resultado es: " + resultado + "\n");
+
+                    break;
+
+                }
+
+                case 4: {
+                    System.out.print("\nIndique la base real: ");
+                    double base = scanner.nextDouble();
+                    System.out.print("Indique el exponente real: ");
+                    double exponente = scanner.nextDouble();
+
+                    Producto producto = new Producto();
+                    double resultado = producto.potencia(base, exponente);
+                    System.out.print("El resultado es: " + resultado + "\n");
+
+                    break;
+
+                }
+                case 5: {
+                    break;
+                }
+
+                default: {
+                    System.out.print("\nNO EXISTE ESE CALCULO\n");
+                    break;
+
+                }
+            }
+
+        } while (opcion3 != 5);
+
+
+    }
+
+    private static void menuCociente() {
+
+
     }
 }
