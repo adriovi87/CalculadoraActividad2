802efe6e3435795abf286e32aaaa483fd6a68c3c
Pablo-Garridolop <pablo.garrido838@comunidadunir.net>
2025-02-24 16:41:23 +0100

    creo la carpeta docs con la documentación del javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Cociente"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Producto"},{"p":"org.example","l":"Resta"},{"p":"org.example","l":"Suma"}];updateSearchResults();
